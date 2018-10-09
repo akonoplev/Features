1d1bd649df18e57ed2c10ec3de5195228c7334ea
akonoplev <bringmethedream@mail.ru>
2018-10-09 15:15:43 +0300

    class sizes

---
+++ AdaptiveLayout/AdaptiveLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by Андрей Коноплев on 08.10.2018.
+//  Copyright © 2018 Андрей Коноплев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
