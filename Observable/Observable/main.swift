0df3db3ff5e55427981b04da1ab2d260680386e4
Andrey <konoplev@wamba.com>
2019-06-07 11:06:29 +0300

    Observable

---
+++ Observable/Observable/main.swift
@@ -0,0 +1,83 @@
+//
+//  main.swift
+//  Observable
+//
+//  Created by Andrei Konoplev on 07/06/2019.
+//  Copyright © 2019 Коноплев Андрей. All rights reserved.
+//
+
+import Foundation
+
+final class Observable<T> {
+    typealias Observer = (T)-> Void
+
+    private var observers: [Int: Observer] = [:]
+    private var uniqueID = (0...).makeIterator()
+
+    var value: T {
+        didSet {
+            observers.values.forEach({ $0(value) })
+        }
+    }
+
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func observeSkippedFirst(_ observe: @escaping Observer)-> Disposable {
+        return makeObserve(skipFirst: true, observe)
+    }
+    
+    func removeAll()-> Void {
+        observers.removeAll()
+    }
+}
+
+private extension Observable {
+    func makeObserve(skipFirst: Bool,_ observer: @escaping Observer)-> Disposable {
+        guard let id = uniqueID.next() else {
+            fatalError("Все айдишники кончились")
+        }
+        
+        observers[id] = observer
+        
+        if !skipFirst {
+            observer(value)
+        }
+        
+        let disposable = Disposable { [weak self] in
+            self?.observers[id] = nil
+        }
+        
+        return disposable
+    }
+}
+
+//MARL: Disposable
+
+typealias Disposal = [Disposable]
+
+final class Disposable {
+    private let dispose: ()-> Void
+    
+    init(_ dispose: @escaping ()-> Void) {
+        self.dispose = dispose
+    }
+    
+    deinit {
+        dispose()
+    }
+    
+    func add(to disposal: inout Disposal) {
+        disposal.append(self)
+    }
+}
+
+var a = Observable("Привет")
+let b = Observable("Как дела")
+_ = a.makeObserve(skipFirst: false) { (string) in
+    print(string)
+}
+
+a.value = "ooo"
+a.value = "aaa"
---
+++ autolayout/autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by Коноплев Андрей on 08/04/2019.
+//  Copyright © 2019 Коноплев Андрей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
